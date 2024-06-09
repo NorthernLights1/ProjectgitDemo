3282aacea09ab8e2acd37271ee3a92fec243c71b
NorthernLights1 <temeg242@gmail.com>
2024-06-09 10:27:37 +0300

    My Second Intellij commit and switched line 7

---
+++ src/anotherclass.java
@@ -0,0 +1,4 @@
+public class anotherclass {
+    public static void classmewhat() {
+    System.out.println("What is this ");
+}}
---
+++ src/clasesefrg.java
@@ -0,0 +1,4 @@
+public class clasesefrg {
+    //whatever
+    something:
+}
